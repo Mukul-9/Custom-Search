21be193d80887f84036b83e5b51f1917c15cc679
Mukul Choudhury <mukulc998@gmail.com>
2020-08-12 10:42:55 +0530

    added build

---
+++ build/precache-manifest.ced9c58593bfe80636bc4fff428d4221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386debf7f8d381a0edfe3dbcbfe6fcf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adde18f1145e6b51077b",
+    "url": "/static/css/main.c5616a2d.chunk.css"
+  },
+  {
+    "revision": "1732a100e5d4a0d8f813",
+    "url": "/static/js/2.583531a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.583531a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adde18f1145e6b51077b",
+    "url": "/static/js/main.6df45e96.chunk.js"
+  },
+  {
+    "revision": "ab5c318329baf9092ede",
+    "url": "/static/js/runtime-main.07bc1fb2.js"
+  }
+]);
